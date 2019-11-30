a4eb91e486a8e359135927fc71a9a1be75a85f2a
ShamilMS <shamilms@code.edu.az>
2019-11-30 11:23:06 +0400

    Initial

---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,92 @@
+﻿#define COMBINE_API_VERSIONS
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddApiVersioning(ops =>
+            {
+                // Will return and controllers action that support [ApiVersion("2.0")] attribute
+                ops.DefaultApiVersion = new ApiVersion(2, 0);
+
+                // if api-version is not specified, it will route to a Default Api Version
+                ops.AssumeDefaultVersionWhenUnspecified = true; 
+
+                // => return Http Reponse api-supported-version: (action supported version) 1.0, 1.5, 2.0
+                ops.ReportApiVersions = true;
+
+#if COMBINE_API_VERSIONS
+
+                // Since we want to multiple way to specify api version we use ApiVersionReader.Combine
+                // Options how you where you can specify api-version
+                ops.ApiVersionReader = ApiVersionReader.Combine(new IApiVersionReader[]
+                {
+                    // should be specified in query like ?api-version=1.0
+                    new QueryStringApiVersionReader("api-version"),
+                    // should be specified in query like ?v=1.0
+                    new QueryStringApiVersionReader("v"),
+
+                    // should be specified in HTTP Request Headers
+                    // X-Version: 1.0
+                    new HeaderApiVersionReader("X-Version"),
+                    // Api-Version: 1.5
+                    new HeaderApiVersionReader("Api-Version")
+                });
+#else
+
+                // This is by default
+                // where you can specify version in query
+                // like  https://localhost:5001/api/values?api-version=1.0
+                ops.ApiVersionReader = new QueryStringApiVersionReader("api-version");
+
+                // will override QueryStringApiVersionReader
+                // 
+                 ops.ApiVersionReader = new HeaderApiVersionReader("X-Version");
+#endif
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/V1/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.V1.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("1.5")]
+    [Route("api/[controller]")]
+    // If this route specified this will override configured
+    // QueryApiVersionReader and HeaderApiVersionReader in Startup
+    // and will handle request like 
+    //~/api/1.0/values
+    //~/api/1.5/values
+    //~/api/2.0/values
+    // BUT X-Version:1.0 in HTTP Request won't work and ?api-version=1.0 Won't work as well
+    //[Route("api/{version:apiVersion}/[controller]")]
+    public class ValuesController : Controller
+    {
+        // https://localhost:5001/api/values?api-version=1.0
+        // If ApiVersionReader = QueryStringApiVersionReader() which is by default
+
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        public IActionResult Get1_0()
+            => Ok("From version 1");
+
+
+        // https://localhost:5001/api/values?api-version=1.5
+        // If ApiVersionReader = QueryStringApiVersionReader() which is by default
+        [HttpGet]
+        [MapToApiVersion("1.5")]
+        public IActionResult Get1_5()
+            => Ok("From version 1.5");
+    }
+}
